e657c9dca42d4ead67ad44732318bf9b2b89c45c
mit2u <mitulrawat@gmail.com>
2026-03-08 16:57:12 +0530

    PPT Generator

--- core/models.py
+++ core/models.py
@@ -3,11 +3,11 @@ from django.db.models import Model, CharField, JSONField, IntegerField
 
 
 class SlideGenerationRequest(Model):
-    topic = CharField(null=True,blank=True)
-    content = CharField(null=True,blank=True)
+    topic = CharField(null=True,blank=True,max_length=20)
+    content = CharField(null=True,blank=True,max_length=20)
     slides = JSONField(null=True)
-    font = CharField(null=True,blank=True)
-    color = CharField(null=True,blank=True)
+    font = CharField(null=True,blank=True,max_length=20)
+    color = CharField(null=True,blank=True,max_length=20)
     max_slides = IntegerField(null=True,blank=True,validators = [MaxValueValidator(20)])
     min_slides = IntegerField(null=True,blank=True,validators = [MinValueValidator(1)])
     provider = CharField(null=True,blank=True,choices = [('ollama','ollama'),('gemini','gemini')])
